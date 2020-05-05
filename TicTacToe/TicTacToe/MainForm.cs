8667609a4a28fcac755c8c4ea86857e7ef846ccd
Nikolay Meleshchenko <kolan0216@users.noreply.github.com>
2020-05-05 16:37:07 +0300

    Реализация главной формы (2/2) (игровое поле)

--- TicTacToe/TicTacToe/MainForm.cs
+++ TicTacToe/TicTacToe/MainForm.cs
@@ -11,6 +11,8 @@ namespace TicTacToe
     /// </summary>
     class MainForm : Form
     {
+        private float cellSize;
+        bool turn;
         public MainForm()
         {
             this.FormBorderStyle = FormBorderStyle.FixedToolWindow;
@@ -21,10 +23,64 @@ namespace TicTacToe
             int pvMargin = 80,
                 //Высота основных кнопок
                 pvHeight = 200;
+            //Панель с игровым полем
+            Panel main = new Panel()
+            {
+                Dock = DockStyle.Fill,
+                Visible = false
+            };
+            Label label = new Label()
+            {
+                Size = new Size(200, 30),
+                TextAlign = ContentAlignment.MiddleCenter,
+                Font = new Font("Arial", 15, FontStyle.Regular),
+                Top = 10,
+                Left = (this.ClientSize.Width - 200) / 2
+            };
             //Панель с меню
             Panel menu = new Panel()
             {
                 Dock = DockStyle.Fill
+            };
+            Panel grid = new Panel()
+            {
+                Size = new Size(600, 600),
+                Left = (this.ClientSize.Width - 600) / 2,
+                Top = 50,
+                BorderStyle = BorderStyle.FixedSingle,
+            };
+            grid.Paint += (a, b) =>
+            {
+                cellSize = (float)grid.ClientSize.Width / (float)Settings.Size;
+                using (var p = new Pen(Color.Black, 1))
+                {
+                    for (int i = 1; i < Settings.Size; i++)
+                    {
+                        DrawLine(p, grid, new PointF(i * cellSize, 0), new PointF(i * cellSize, grid.ClientSize.Height));
+                        DrawLine(p, grid, new PointF(0, i * cellSize), new PointF(grid.ClientSize.Width, i * cellSize));
+                    }
+                }
+                Label(label);
+            };
+            Button back = new Button()
+            {
+                Text = "Назад",
+                Width = grid.Width / 2 - 10,
+                Height = 50,
+                Font = new Font("Arial", 15, FontStyle.Regular),
+                Top = grid.Bottom + 25,
+                Left = grid.Left,
+                FlatStyle = FlatStyle.Flat
+            };
+            Button restart = new Button()
+            {
+                Text = "Рестарт",
+                Width = grid.Width / 2 - 10,
+                Height = 50,
+                Font = new Font("Arial", 15, FontStyle.Regular),
+                Top = grid.Bottom + 25,
+                Left = back.Right + 20,
+                FlatStyle = FlatStyle.Flat
             };
             Button pve = new Button()
             {
@@ -81,6 +137,40 @@ namespace TicTacToe
             menu.Controls.Add(help);
             menu.Controls.Add(exit);
             this.Controls.Add(menu);
+        }
+        /// <summary>
+        /// Рисует линию на панеле через две точки.
+        /// </summary>
+        /// <param name="p">Кисть</param>
+        /// <param name="pnl">Панель</param>
+        /// <param name="p1">Начальная точка</param>
+        /// <param name="p2">Конечная точка</param>
+        public void DrawLine(Pen p, Panel pnl, PointF p1, PointF p2)
+        {
+            using (var g = pnl.CreateGraphics())
+            {
+                g.DrawLine(p, p1, p2);
+            }
+        }
+        /// <summary>
+        /// Меняет текст метки с текущим ходом.
+        /// </summary>
+        /// <param name="label">Метка с текущим ходом</param>
+        public void Label(Label label)
+        {
+            switch (Game.Side)
+            {
+                case Side.X:
+                    label.Text = "Ходят крестики";
+                    label.ForeColor = Settings.XColor;
+                    break;
+                case Side.O:
+                    label.Text = "Ходят нолики";
+                    label.ForeColor = Settings.OColor;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
